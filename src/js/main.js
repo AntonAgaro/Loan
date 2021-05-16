9ac040cde75a6436c0b28f009c765409975f9fe6
Антон Агаронов <antonagaronov@MacBook-Air-Anton.local>
2021-05-16 20:56:19 +0400

    feat: add slider on modules page

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
     slider.render();
 
+    const modulepPageSlider = new MainSlider({container: '.moduleapp', btns: '.next'});
+    modulepPageSlider.render();
+
     const showUpSlider = new MiniSlider({ 
         container: '.showup__content-slider',
         prev: '.showup__prev',
